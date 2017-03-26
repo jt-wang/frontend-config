c854e059b46b6c51ca49977404a18f5d7cd8a43e
wjt <jtwang.me@gmail.com>
2017-03-26 19:27:33 +0800

    config of webpack, babel, and eslint

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './app/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    hot: true,
+
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ['transform-runtime']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Three.js App'
+    })
+  ]
+};
+
+module.exports = config;
